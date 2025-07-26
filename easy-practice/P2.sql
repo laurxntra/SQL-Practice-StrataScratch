0e84ca808aa2fc233edd7deee9501502c9fe9a0f
Lauren Tra <53579807+laurxntra@users.noreply.github.com>
2025-07-25 21:40:10 -0700

    Create P2.sql

---
+++ easy-practice/P2.sql
@@ -0,0 +1,5 @@
+SELECT COUNT(*)
+FROM oscar_nominees
+WHERE nominee = 'Abigail Breslin'
+
+-- https://platform.stratascratch.com/coding/10128-count-the-number-of-movies-that-abigail-breslin-nominated-for-oscar?code_type=5
