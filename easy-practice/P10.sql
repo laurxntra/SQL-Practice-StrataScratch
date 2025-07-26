fe790e450ede55fd66b84446178ed9063f2dd162
Lauren Tra <53579807+laurxntra@users.noreply.github.com>
2025-07-25 22:44:19 -0700

    Create P10.sql

---
+++ easy-practice/P10.sql
@@ -0,0 +1,5 @@
+SELECT activity_date, pe_description
+FROM los_angeles_restaurant_health_inspections
+WHERE facility_name = 'STREET CHURROS' AND score < 95
+
+-- https://platform.stratascratch.com/coding/9688-churro-activity-date?code_type=5
